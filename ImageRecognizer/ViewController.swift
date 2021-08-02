c1b64a3aee95c37b657ebf2893a57b56293ba774
tiagogomez <tiago.gomezg@ggail.com>
2021-08-02 11:25:54 -0500

    Using Core ML to recognize image

--- ImageRecognizer/ViewController.swift
+++ ImageRecognizer/ViewController.swift
@@ -13,16 +13,47 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     
+    var resnetModel = Resnet50()
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        var resnetModel = Resnet50()
+        imagePicker.delegate = self
         
         guard let image = imageView.image else { return }
         classifyPicture(image: image)
     }
+    @IBAction func albumTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func photoTapped(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
     
     private func classifyPicture(image: UIImage) {
+        guard let model = try? VNCoreMLModel(for: resnetModel.model) else { return }
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation] else { return }
+            let result = results[0]
+            self.navigationController?.navigationBar.topItem?.title = result.identifier
+        }
+        guard let imageData = image.jpegData(compressionQuality: 1.0) else { return }
+        let handler = VNImageRequestHandler(data: imageData, options: [:])
+        try? handler.perform([request])
         
     }
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.originalImage] as? UIImage else { return }
+        imageView.image = image
+        classifyPicture(image: image)
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
